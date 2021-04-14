a63414ead3108337d38a63ca06adef7706ebe872
BidaultArmand <74368981+BidaultArmand@users.noreply.github.com>
2021-04-14 22:47:43 +0900

    uppdate3

--- Components/PlatItem.js
+++ Components/PlatItem.js
@@ -1,87 +1,86 @@
-import React from 'react'
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 
-class PlatItem extends React.Component {
-  render() {
-    const{ plat, displayDetailForPlat } = this.props
-    return (
-      <TouchableOpacity
-        onPress={() => displayDetailForPlat(plat.id)}
-        style={styles.main_container}
-        underlayColor='#37401a'>
-        <Image
-          style={styles.image}
-          source={{uri: "image"}}
-        />
-        <View style={styles.content_container}>
-          <View style={styles.header_container}>
-            <Text style={styles.title_text}>{plat.title}</Text>
-            <Text style={styles.localisation}>{plat.localisation}</Text>
-          </View>
-          <View style={styles.description_container}>
-            <Text style={styles.description_text} numberOfLines={6}>{plat.description}</Text>
-            
-          </View>
-          <View style={styles.date_container}>
-            <Text style={styles.price}>{plat.price}</Text>
-          </View>
+function PlatItem(props) {
+  console.log(props);
+  const { plat, displayDetailForPlat } = props;
+
+  return (
+    <TouchableOpacity
+      onPress={() => props.navigation.navigate("OrderMenue")}
+      style={styles.main_container}
+      underlayColor="#37401a"
+    >
+      <Image style={styles.image} source={{ uri: "image" }} />
+      <View style={styles.content_container}>
+        <View style={styles.header_container}>
+          <Text style={styles.title_text}>{plat.title}</Text>
+          <Text style={styles.localisation}>{plat.localisation}</Text>
         </View>
-        <View style={styles.line}></View>
-      </TouchableOpacity>
-    )
-  }
+        <View style={styles.description_container}>
+          <Text style={styles.description_text} numberOfLines={6}>
+            {plat.description}
+          </Text>
+        </View>
+        <View style={styles.date_container}>
+          <Text style={styles.price}>{plat.price}</Text>
+        </View>
+      </View>
+      <View style={styles.line}></View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
   main_container: {
     height: 500,
-    flexDirection: 'column'
+    flexDirection: "column",
   },
   image: {
     width: "100%",
     height: 250,
-    backgroundColor: 'gray'
+    backgroundColor: "gray",
   },
   content_container: {
     flex: 1,
-    margin: 5
+    margin: 5,
   },
   header_container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: "column",
   },
   title_text: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 26,
     flex: 1,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     paddingRight: 5,
-    color: "#37401a"
+    color: "#37401a",
   },
   localisation: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 20,
-    color: '#666666',
-    flex: 1
+    color: "#666666",
+    flex: 1,
   },
   description_container: {
-    flex: 1
+    flex: 1,
   },
   description_text: {
-    fontStyle: 'italic',
-    color: '#666666'
+    fontStyle: "italic",
+    color: "#666666",
   },
   date_container: {
-    flex: 1
+    flex: 1,
   },
   price: {
-    textAlign: 'center',
-    fontSize: 14
+    textAlign: "center",
+    fontSize: 14,
   },
   line: {
     height: 2,
-    backgroundColor: '#707070'
-  }
-})
+    backgroundColor: "#707070",
+  },
+});
 
-export default PlatItem
+export default PlatItem;
