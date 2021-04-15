ced06c35855c3c29bc0b31d6079ca7ad58da063d
BidaultArmand <74368981+BidaultArmand@users.noreply.github.com>
2021-04-15 15:55:14 +0900

    uppdate4_navigator_mistake

--- App.js
+++ App.js
@@ -1,49 +1,118 @@
-import { Image } from 'react-native';
-import React from 'react';
+import { Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import PageAccueil from "./Screen/PageAccueil";
 import PageInscription from "./Screen/PageInscription";
 import NavigationBar from "./Screen/Tabnavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profil from "./Screen/Profil";
-import Search from './Screen/Search';
-import Cook from './Screen/Cook';
-import PlatItem from './Components/PlatItem';
-import PlatDetail from './Components/PlatDetail';
+import Search from "./Screen/Search";
+import Cook from "./Screen/Cook";
+import PlatItem from "./Components/PlatItem";
+import PlatDetail from "./Components/PlatDetail";
+import AppContext from "./Components/AppContext";
 
 const AppStack = createStackNavigator();
 const AppTab = createBottomTabNavigator();
+const AppStackOrder = createStackNavigator();
 function App() {
+  //GlobalState
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+  //Variable Accessible
+  const Parameters = {
+    UserLogged: isLoggedIn,
+    SetIsLoggedIn,
+  };
   return (
-    <NavigationContainer>
-      <AppStack.Navigator screenOptions={{headerShown: false}}>
-        
-        <AppStack.Screen name="PageInscription" component={PageInscription}/>
-        <AppStack.Screen name="Tabnavigator" component={TabNavigator}/>
-        <AppStack.Screen name="PlatDetail" component={PlatDetail}/>
-      </AppStack.Navigator>
-    </NavigationContainer>
-  )
+    <AppContext.Provider value={Parameters}>
+      <NavigationContainer>
+        <AppStack.Navigator screenOptions={{ headerShown: false }}>
+          {!isLoggedIn ? (
+            <AppStack.Screen
+              name="PageInscription"
+              component={PageInscription}
+            />
+          ) : (
+            <AppStack.screen name="Navigator2" component={Navigator2} />
+          )}
+        </AppStack.Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
+  );
 }
 function TabNavigator() {
-  return(
+  return (
     <AppTab.Navigator>
-        <AppTab.Screen name="Accueil" component={PageAccueil} 
-          options={{tabBarIcon: () => <Image source={require("./assets/Accueil.png")} style={{ height: 30, }}/>}} 
-        />
-        <AppTab.Screen name="Recherche" component={Search}
-         options={{tabBarIcon: () => <Image source={require("./assets/Search.png")} style={{ height: 30, }}/>}} 
-         />
-        <AppTab.Screen name="Cook" component={Cook} 
-        options={{tabBarIcon: () => <Image source={require("./assets/Cook.png")} style={{ height: 30, }}/>}} 
-        />
-        <AppTab.Screen name="Profil" component={Profil} 
-        options={{tabBarIcon: () => <Image source={require("./assets/Profil.png")} style={{ height: 30, }}/>}} 
-        />
-      </AppTab.Navigator>
-  )
+      <AppTab.Screen
+        name="Accueil"
+        component={PageAccueil}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/Accueil.png")}
+              style={styles.image}
+            />
+          ),
+        }}
+      />
+      <AppTab.Screen
+        name="Recherche"
+        component={Search}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/Search.png")}
+              style={styles.image}
+            />
+          ),
+        }}
+      />
+      <AppTab.Screen
+        name="Cook"
+        component={Cook}
+        options={{
+          tabBarIcon: () => (
+            <Image source={require("./assets/Cook.png")} style={styles.image} />
+          ),
+        }}
+      />
+      <AppTab.Screen
+        name="Profil"
+        component={Profil}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/Profil.png")}
+              style={styles.image}
+            />
+          ),
+        }}
+      />
+    </AppTab.Navigator>
+  );
+}
+function OrderMenue() {
+  return (
+    <AppStackOrder.Navigator>
+      <AppStackOrder.screen name="PlatDetail" component={PlatDetail} />
+    </AppStackOrder.Navigator>
+  );
+}
+function Navigator2() {
+  return (
+    <AppStackNavigator2.Navigator>
+      <AppStackNavigator2.Screen name="Tabnavigator" component={TabNavigator} />
+      <AppStackNavigator2.screen name="PlatDetail" component={PlatDetail} />
+    </AppStackNavigator2.Navigator>
+  );
 }
 
-export default App
-
+const styles = StyleSheet.create({
+  image: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "contain",
+  },
+});
+export default App;
